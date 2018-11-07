301508b34692029a9d064ff23f582bf47d52655b
GilVicttor <33091208+GilVicttor@users.noreply.github.com>
2018-11-07 00:44:00 -0200

    Create Prova.java

---
+++ Prova.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+
+public class Prova {
+
+    private ArrayList<Pergunta> perguntas;
+    
+    public Prova(){
+        this.perguntas = new ArrayList<Pergunta>();
+    };
+
+    public ArrayList<Pergunta> getPerguntas() {
+        return this.perguntas;
+    }
+
+    public void setPerguntas(ArrayList<Pergunta> perguntas) {
+        this.perguntas = perguntas;
+    }
+    public void addPerguntas(Pergunta x){
+        this.perguntas.add(x);
+        
+    }
+    public void imprimirQuestoes(){
+        int i= 1;
+        for (Pergunta p: perguntas){
+            System.out.println("QSTS: " +i++ +" " +p.getDescricao());
+        }
+       }
+    public void imprimirGabarito(){
+        int a= 1;
+        for(Pergunta j : perguntas){
+            System.out.println("RESPOSTAS: " +a++ +")"+j.getResposta());
+        }
+    }    
+        
+    }
+    
+    
