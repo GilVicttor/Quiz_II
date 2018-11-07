7fecbeada44126c8e425c3c152e7423cc3b95c1b
GilVicttor <33091208+GilVicttor@users.noreply.github.com>
2018-11-07 00:42:33 -0200

    Create PerguntaMultEscolha.java

---
+++ PerguntaMultEscolha.java
@@ -0,0 +1,15 @@
+public class PerguntaMultEscolha extends Pergunta{
+    private String alternativaCorreta;
+
+    public void setAlternativaCorreta(String alternativaCorreta) {
+        this.alternativaCorreta = alternativaCorreta;
+    }
+    
+    
+    @Override
+    public String getResposta() {
+        return "alternativa: " +this.alternativaCorreta;
+    }
+    
+    
+}
